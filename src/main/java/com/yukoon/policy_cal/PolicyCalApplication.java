e9fde016dd6fb54d1f8b98a6a527ea35a65afdc1
a5928497 <61722615@qq.com>
2018-04-29 11:36:17 +0800

    create project

---
+++ src/main/java/com/yukoon/policy_cal/PolicyCalApplication.java
@@ -0,0 +1,12 @@
+package com.yukoon.policy_cal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyCalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicyCalApplication.class, args);
+	}
+}
---
+++ src/test/java/com/yukoon/policy_cal/PolicyCalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yukoon.policy_cal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PolicyCalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
