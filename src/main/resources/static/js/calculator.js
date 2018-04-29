c46be3727500716e702c4f93f1cd1a66405ec676
a5928497 <61722615@qq.com>
2018-04-29 14:14:22 +0800

    developing

--- src/main/resources/static/js/calculator.js
+++ src/main/resources/static/js/calculator.js
@@ -1,3 +1,16 @@
 $(function () {
+    var $isSingle =  $("#isSingle")
+    var $pd_amount = $("#pd_amount")
 
+    $pd_amount.hide();
+    //选择多个商品时显示数目
+    $isSingle.change(function () {
+        var $opt_select = $(this).find("option:selected").val();
+        if ($opt_select == "0") {
+            $pd_amount.show();
+        }
+        if ($opt_select == "1") {
+            $pd_amount.hide();
+        }
+   })
 })
