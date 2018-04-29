1ea029372d864e1c0496ba16c760be69e379881a
a5928497 <61722615@qq.com>
2018-04-29 14:30:46 +0800

    developing

--- src/main/resources/static/js/calculator.js
+++ src/main/resources/static/js/calculator.js
@@ -1,6 +1,8 @@
 $(function () {
-    var $isSingle =  $("#isSingle")
-    var $pd_amount = $("#pd_amount")
+    var $isSingle =  $("#isSingle");
+    var $pd_amount = $("#pd_amount");
+    var $amount_num = $("#amount_num");
+    var $std = $("#std_container");
 
     $pd_amount.hide();
     //选择多个商品时显示数目
@@ -11,6 +13,18 @@ $(function () {
         }
         if ($opt_select == "1") {
             $pd_amount.hide();
+            $amount_num.val("1")
+            $std.empty();
+            $std.append(" <label>商品规格：1*</label><input type=\'text\' size=\'1\'><br>");
         }
    })
+
+    //根据商品数目添加商品规格
+    $amount_num.change(function () {
+        var times = parseInt($(this).val());
+        $std.empty();
+        for (var i = 0;i<times;i++){
+            $std.append(" <label>商品规格：1*</label><input type=\'text\' size=\'1\'><br>");
+        }
+    })
 })
