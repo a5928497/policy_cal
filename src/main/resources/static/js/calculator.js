097ccb5b131bdfbbb91639c615a9a7c842996809
a5928497 <61722615@qq.com>
2018-04-29 15:14:10 +0800

    developing

--- src/main/resources/static/js/calculator.js
+++ src/main/resources/static/js/calculator.js
@@ -3,8 +3,25 @@ $(function () {
     var $pd_amount = $("#pd_amount");
     var $amount_num = $("#amount_num");
     var $std = $("#std_container");
-
+    var $MorP = $(".MorP");
+    //初始隐藏
     $pd_amount.hide();
+
+    //选择赠送则显示送，选择减钱则显示减
+    $MorP.change(function () {
+        var $MorP_select = $(this).find("option:selected").val();
+        var span_num = "#statu" + $(this).attr("name").substring(4,7);
+        var $span = $(span_num);
+        //等于0，减钱
+        if ($MorP_select == "0") {
+            $span.html("减")
+        }
+        //等于1，赠送
+        if ($MorP_select == "1") {
+            $span.html("送")
+        }
+    })
+
     //选择多个商品时显示数目
     $isSingle.change(function () {
         var $opt_select = $(this).find("option:selected").val();
